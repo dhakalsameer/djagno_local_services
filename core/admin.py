e79faf3b3d8dac262c84da5ce115b6300f2a17cd
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-01-26 12:38:44 +0545

    register models in the admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(ServiceCategory)
+admin.site.register(Service)
+admin.site.register(ServiceImage)
+admin.site.register(ServiceAvailability)
+admin.site.register(Booking)
+admin.site.register(Review)
