25966f55c1e744c01eb8eac01bb7da4904e7463f
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-01-26 14:02:28 +0545

    update the settings.py

--- config/settings.py
+++ config/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
@@ -125,3 +125,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets',
+]
