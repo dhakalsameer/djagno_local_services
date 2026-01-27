db9aba77591ec5f1150bcf9594dc7215662c72b0
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-01-27 20:52:39 +0545

    changing the profile pictures logic in the navbar.html

--- core/forms.py
+++ core/forms.py
@@ -16,9 +16,11 @@ class ServiceForm(forms.ModelForm):
         fields = ['category', 'title', 'description', 'price', 'price_type', 'location', 'is_active']
 
     def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)  # <- handle user here
         super().__init__(*args, **kwargs)
         # Sort categories alphabetically
         self.fields['category'].queryset = ServiceCategory.objects.all().order_by('name')
+
 
 
 
