ff56f3e82341b213e1249e3b0e17c253103a9b40
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-01-30 10:14:39 +0545

    making the Review model

--- core/models.py
+++ core/models.py
@@ -100,22 +100,12 @@ class Booking(models.Model):
 
 # ------------------ Review ------------------
 class Review(models.Model):
-    booking = models.OneToOneField(
-    Booking,
-    on_delete=models.CASCADE,
-    related_name='review',
-)
-
-    service = models.ForeignKey(
-        Service,
-        on_delete=models.CASCADE,
-        related_name='reviews'
-    )
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='reviews')
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
-
     rating = models.PositiveSmallIntegerField()
     comment = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.service.title} - {self.rating}⭐"
+        return f"{self.service.title} - {self.rating}"
